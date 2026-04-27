7ee66517ed871ec40f386599632c6f8b7299126e
Toghrul <ti-togrul@mail.ru>
2026-04-27 19:15:46 +0400

    stale time added

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,36 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; 
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // Datalar 5 dəqiqə "təzə" qalsın (təkrar sorğu atmasın)
+      gcTime: 1000 * 60 * 10, // Keşdə 10 dəqiqə saxla
+      refetchOnWindowFocus: false, // Pəncərəyə qayıdanda təzədən yükləmə (Trafikə qənaət)
+    },
+  },
+});
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client = {queryClient}>
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-     <ToastContainer 
+    <ToastContainer
       position="top-center" // Mobildə tam eni tutması üçün bu daha yaxşıdır
-  autoClose={3000}
-  hideProgressBar={false}
-  newestOnTop={false}
-  closeOnClick
-  rtl={false}
-  pauseOnFocusLoss
-  draggable
-  pauseOnHover
-  theme="colored"
-  
-  // Mobildə kənarlardakı boşluqları silmək üçün:
-  style={{ width: "100%" }} 
-      
+      autoClose={3000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+      // Mobildə kənarlardakı boşluqları silmək üçün:
+      style={{ width: "100%" }}
     />
   </QueryClientProvider>,
-)
+);
